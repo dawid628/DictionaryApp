112bd016cc9fdb71288cc046cc5320400e9d9f2b
Dawid Metelski <metel628@onet.pl>
2023-09-07 19:09:39 +0200

    Create dtos for Word and Definition

---
+++ app/Models/Dtos/DefinitionDTO.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\DTO;
+
+class DefinitionDTO
+{
+    public $word_id;
+    public $body;
+}
---
+++ app/Models/Dtos/WordDTO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\DTO;
+
+class WordDTO
+{
+    public $name;
+    public $tags;
+    public $active;
+    public $definition;
+    public $created_at;
+    public $updated_at;
+}
