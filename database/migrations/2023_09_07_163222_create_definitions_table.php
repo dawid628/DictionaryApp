5051673e8e1398b9912ca94c3098a77f6d5d385d
Dawid Metelski <metel628@onet.pl>
2023-09-07 18:43:14 +0200

    Create models and migrations for words and definitions

---
+++ database/migrations/2023_09_07_163222_create_definitions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('definitions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('word_id')->unique();
+            $table->text('body');
+            $table->timestamps();
+            
+            $table->foreign('word_id')->references('id')->on('words')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('definitions');
+    }
+};
